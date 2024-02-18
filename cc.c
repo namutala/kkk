acebb5253a311034145e0993dfd71731e5e070f7
namutala <160399571+namutala@users.noreply.github.com>
2024-02-18 15:05:00 +0300

    Add files via upload

---
+++ cc.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+	
+     float a,b,c,sum,average;
+     
+     
+	 printf("Please input the results for the a assignment: ");
+	 scanf("%d", &a);	
+	 printf("Please input the results for b assignment: ");
+	 scanf("%d", &b);
+	 printf("Please  input the results for the c assignment: ");
+	 scanf("%d", &b);
+	sum=a+b+c;
+	printf("%d",sum);
+		
+	
+	
+}
